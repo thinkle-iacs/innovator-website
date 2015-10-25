9d5c45c416ccbd615e9854410de47cddc99fa077
Thomas M. Hinkle <tmhinkle@gmail.com>
2015-10-25 16:08:24 -0400

    Add sports, exclude highlights from being listed twice, adjust header

--- home.php
+++ home.php
@@ -17,8 +17,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		  <div id="highlighted">
-		    <h1>Latest</h1>
+		  <div id="highlighted" class="layout-region">
+		    <h2>Latest</h1>
 		      <?php $query = new WP_Query( array( 'category_name' => 'highlight' ) ); ?>
 		      
 		      <?php /* Start the Loop */ ?>
@@ -40,9 +40,10 @@ get_header(); ?>
 					     
 		    </div>
 		    
-		<div id="region-two">
+		<div id="region-two" class="layout-region">
 		  <h2>News</h2>
-		  <?php $query2 = new WP_Query( array( 'category_name' => 'news' ) ); ?>
+		  <?php $query2 = new WP_Query( array( 'category_name' => 'news',
+											                  			    'category__not_in' => array( 13 ) )); ?>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( $query2->have_posts() ) : $query2->the_post(); ?>
@@ -60,10 +61,35 @@ get_header(); ?>
 			<?php endwhile; ?>
 
 			<?php the_posts_navigation(); ?>
+
+		  <h2>Sports</h2>
+
+		  <?php $query2 = new WP_Query( array( 'category_name' => 'sports',
+											                  			    'category__not_in' => array( 13 ) )); ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( $query2->have_posts() ) : $query2->the_post(); ?>
+
+				<?php
+
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_format() );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
+
 		</div>
-		<div id="region-three">
+		<div id="region-three" class="layout-region">
 		  <h2>Opinion</h2>
-		  <?php $query3 = new WP_Query( array( 'category_name' => 'opinion' ) ); ?>
+		  <?php $query3 = new WP_Query( array( 'category_name' => 'opinion',
+											                  			    'category__not_in' => array( 13 )
+          ) ); ?>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( $query3->have_posts() ) : $query3->the_post(); ?>
@@ -82,9 +108,10 @@ get_header(); ?>
 
 			<?php the_posts_navigation(); ?>
 		</div>
-		<div id="region-four">
+		<div id="region-four" class="layout-region">
 		  		  <h2>Arts</h2>
-		  <?php $query4 = new WP_Query( array( 'category_name' => 'arts' ) ); ?>
+		  <?php $query4 = new WP_Query( array( 'category_name' => 'arts',
+											                  			    'category__not_in' => array( 13 ) )); ?>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( $query4->have_posts() ) : $query4->the_post(); ?>
@@ -104,9 +131,10 @@ get_header(); ?>
 			<?php the_posts_navigation(); ?>
 			
 		</div>
-		<div id="region-five">
+		<div id="region-five" class="layout-region">
 		  		  <h2>Features</h2>
-		  <?php $query5 = new WP_Query( array( 'category_name' => 'features' ) ); ?>
+		  <?php $query5 = new WP_Query( array( 'category_name' => 'features',
+											                  			    'category__not_in' => array( 13 ) )); ?>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( $query5->have_posts() ) : $query5->the_post(); ?>
@@ -138,7 +166,7 @@ get_header(); ?>
 <!-- we probably shouldn't be shoving javascript down here, but it works -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script>
-  $(document).ready(function () {
+	function update_layout () {
       console.log('hello world!');
         if ($('#highlighted').css('position') == 'absolute') { // Test whether we are using the fancy absolute layout
 	    var hl_bottom = $('#highlighted').height()
@@ -151,9 +179,32 @@ get_header(); ?>
             if ($('#region-five').height() > total_bottom) {total_bottom = $('#region-five').height()}
             console.log('put footer at ',total_bottom);
             $('footer#colophon').css({'margin-top':total_bottom})
+						console.log('Adjust all our columns to be the right size...');
+						// We should probably move this to styles.css...
+						$('article').css({'border':'none'});
+						$('#region-two').css({'border-left':'1px #c6093b solid '})
+						$('#region-five').css({'border-right':'1px #c6093b solid '})
+						$('#region-three').css({'border-right':'1px #c6093b solid '})
+						$('#highlighted').css({'border-bottom':'1px #c6093b solid '})
+						// Now adjust heights
+						console.log('heights - ',total_bottom)
+						$('#region-two').css({'height':total_bottom})
+						$('#region-five').css({'height':total_bottom})
+						var secondary_height = total_bottom - hl_bottom
+						console.log('secondary height - ',secondary_height)
+						$('#region-three').css({'height':secondary_height})
+						$('#region-four').css({'height':secondary_height})
         }
        else {
          console.log('not in absolute mode')
+				 $('#region-two').css({'height':'auto'})
+				 $('#region-five').css({'height':'auto'})
+				 $('#region-three').css({'height':'auto'})
+				 $('#region-four').css({'height':'auto'})
        }
-    })
+    }
+		$(document).ready(function () {
+															 update_layout();
+															 $(window).resize(update_layout);
+															 })
 </script>
