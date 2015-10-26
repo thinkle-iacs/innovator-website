e33467955742f31602ff571cf2fd347b5da464ec
Thomas M. Hinkle <tmhinkle@gmail.com>
2015-10-26 08:50:09 -0400

    remote hello world test :)

--- home.php
+++ home.php
@@ -18,7 +18,6 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		  <div id="highlighted" class="layout-region">
-				HELLO WORLD :)
 		    <h2>Latest</h2>
 		      <?php $query = new WP_Query( array( 'category_name' => 'highlight' ) ); ?>
 		      
